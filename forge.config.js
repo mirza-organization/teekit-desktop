a0ddbcb167c2751787a676dca96bc69a4cea466a
Muhammad Abdullah Mirza <abdullah.izhar@teo-intl.com>
2024-07-14 14:12:06 +0500

    Teekit desktop app released

---
+++ forge.config.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7a529639275d9aa98a9ed9e30655f9bfe8e4125049b17229409bb289a22fd693
+size 1420
---
+++ main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0f4b4650615c296a1a289040c6ef2c067c41eac9863340c52113d49bff2e242
+size 664
